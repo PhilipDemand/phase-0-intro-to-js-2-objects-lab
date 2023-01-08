fdd8e7c4f6c896207785ac1d6570f34ed3bf0584
Philip Demand <philipdemand@yahoo.com>
2023-01-07 19:05:43 -0500

    commit tree 5c238a48fe3d7d937989aefad6bcbd59cf42682f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Johnny Johnson',
+    address: '38 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObject = {...employee}
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newerObject = {...object}
+    delete newerObject[key]
+    return newerObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+  }
+
